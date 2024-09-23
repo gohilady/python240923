ffc8e26ff17c7371e42ddf732797aaa63562d16e
gohilady <gohilady1@gmail.com>
2024-09-23 16:20:50 +0900

    함수 연습

--- funtion1.py
+++ funtion1.py
@@ -17,3 +17,42 @@ def swap(x,y):
 
 #호출
 print(swap(3,4))
+
+#교집합 문자 리턴
+def intersect(prelist, postlist):
+    #지역변수
+    result = []
+
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
+    
+#호출
+print(intersect('HAM','SPAM'))
+
+#기본값을 명시
+def times(a=10, b=20):
+    return a*b
+
+print(times())
+print(times(5))
+print(times(5,6))
+
+
+#지역변수와 전역변수
+x=5
+def func1(a):
+    return a+x
+
+#호출
+print(func1(1))
+
+def func2(a):
+    #지역변수
+    x=1
+    return a+x
+
+#호출
+print(func2(1))
+
